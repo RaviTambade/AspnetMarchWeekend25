dd404f0aba7bd1f44c9871454ed706468bb3b746
Ravi Tambade <ravi.tambade@transflower.in>
2025-04-12 11:22:07 +0530

    week 6

---
+++ Solutions/Week6/Week6Solution/BIWebApp/Controllers/GraphController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BIWebApp.Controllers
+{
+    public class GraphController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Bar()
+        {
+            //get data from database
+            //get from services---repository pattern---database (JSON, SQL, etc)
+            int[] data = { 1200, 75000, 50000, 25000, 300, 15 };
+            ViewData["data"] = data;
+            return View();
+        }
+
+        public IActionResult Comp()
+        {
+            return View();
+        }
+    }
+}
---
+++ Solutions/Week6/Week6Solution/BIWebApp/Controllers/TagController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BIWebApp.Controllers
+{
+    public class TagController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+        public IActionResult Danger()
+        {
+            return View();
+        }
+
+    }
+}
