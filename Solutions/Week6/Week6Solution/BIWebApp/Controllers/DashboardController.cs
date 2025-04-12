865d4ce94e49441e578d46c428bcb7bc52134dcb
Ravi Tambade <ravi.tambade@transflower.in>
2025-04-12 13:58:43 +0530

    dashboard controller  using ajax

---
+++ Solutions/Week6/Week6Solution/BIWebApp/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using BIWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BIWebApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(); // Chart loads from JS
+        }
+
+        [HttpGet]
+        public IActionResult GetChartData()
+        {
+            var data = new ChartDataViewModel
+            {
+                Labels = new List<string> { "10:00", "10:05", "10:10", "10:15" },
+                Values = new List<int> { 120, 150, 170, 200 }
+            };
+
+            return Json(data);
+        }
+    }
+}
