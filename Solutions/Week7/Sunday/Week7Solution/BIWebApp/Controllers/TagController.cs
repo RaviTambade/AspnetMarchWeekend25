fec599057453846f45bb39bca77ab4c9eeaf4f7c
Ravi Tambade <ravi.tambade@transflower.in>
2025-04-20 11:19:55 +0530

    asynchronous programming using async/await

---
+++ Solutions/Week7/Sunday/Week7Solution/BIWebApp/Controllers/TagController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BIWebApp.Controllers
+{
+    public class TagController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            int workerThreadID = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"Thread ID: {workerThreadID}");
+
+            return View();
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+        public IActionResult Danger()
+        {
+            return View();
+        }
+
+        public IActionResult Warn()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Solutions/Week7/Sunday/Week7Solution/BIWebApp/Models/ChartDataViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BIWebApp.Models
+{
+    public class ChartDataViewModel
+    {
+        public List<string> Labels { get; set; }
+        public List<int> Values { get; set; }
+    }
+}
