7d73bef218f6081cbe2b17d78898ad8f518abeed
Ravi Tambade <ravi.tambade@transflower.in>
2025-05-25 11:08:12 +0530

    pune app

---
+++ Solutions/Week12/FromPuneWebApp/FromPuneWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FromPuneWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
